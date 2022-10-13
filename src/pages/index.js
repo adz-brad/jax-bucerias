2a9d5eb03dea3aa22d6da2587d2d7079509e3e33
Brad S <brad@adrenalizedigital.ca>
2022-10-13 15:36:41 -0600

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+
+  return(
+
+    <div>
+
+    </div>
+    
+  )
 }
+
+export default Home
