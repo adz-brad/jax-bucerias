bd3131dd0635b688a0e4eb8b7b6f8840ce2716b1
Brad S <brad@adrenalizedigital.ca>
2022-10-13 22:07:03 -0600

    route #menu to /menu/

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { navigate } from "gatsby"
 
 const Home = () => {
+
+  const hash = typeof window !== 'undefined' ? window.location.hash : null
+
+  useEffect(() => {
+    if(hash === '#menu' ){
+      navigate("/menu/", { replace: true })
+    }
+  }, [ hash ])
 
   return(
 
